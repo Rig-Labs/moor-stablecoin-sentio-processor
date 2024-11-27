76db577f187b4b6b7fd1274ca9ed56b12653805f
meirbank <meir@meirbank.com>
2024-11-27 16:29:29 +0700

    add trove adjusted

--- fluid-protocol/src/processor.ts
+++ fluid-protocol/src/processor.ts
@@ -1,9 +1,9 @@
 import { LogLevel } from '@sentio/sdk'
 import { FuelNetwork } from '@sentio/sdk/fuel'
-
+import { GLOBAL_CONFIG } from '@sentio/runtime';
+import { BigDecimal } from '@sentio/sdk';
+import { getPriceBySymbol } from '@sentio/sdk/utils';
 import { BorrowerOperationsContractProcessor } from './types/fuel/BorrowerOperationsContractProcessor.js'
-
-
 
 BorrowerOperationsContractProcessor.bind({
   address: '0xeda02ae9cbe68183ee86a0b5e7a475215fbafc307e5e6506f970fa6dcc15d62e', //mainnet production 
@@ -24,11 +24,35 @@ BorrowerOperationsContractProcessor.bind({
   )
   .onLogOpenTroveEvent(async (log, ctx) => {
     let timestamp = ctx.block?.time
-    // you can also call ctx.contract.functions.complex(...)
-    ctx.eventLogger.emit('troveOpened', { user: String(log.data.user), asset_id: String(log.data.asset_id), collateral: String(log.data.collateral), debt: String(log.data.debt), timestamp: String(timestamp) })
+    ctx.eventLogger.emit('troveOpened', {
+      user: String(log.data.user),
+      asset_id: String(log.data.asset_id),
+      collateral: String(log.data.collateral),
+      debt: String(log.data.debt),
+      timestamp: String(timestamp)
+    })
   })
   .onLogCloseTroveEvent(async (log, ctx) => {
     let timestamp = ctx.block?.time
-    // you can also call ctx.contract.functions.complex(...)
-    ctx.eventLogger.emit('troveClosed', { user: String(log.data.user), asset_id: String(log.data.asset_id), collateral: String(log.data.collateral), debt: String(log.data.debt), timestamp: String(timestamp) })
+
+    ctx.eventLogger.emit('troveClosed', {
+      user: String(log.data.user),
+      asset_id: String(log.data.asset_id),
+      collateral: String(log.data.collateral),
+      debt: String(log.data.debt),
+      timestamp: String(timestamp)
+    })
+  })
+  .onLogAdjustTroveEvent(async (log, ctx) => {
+    let timestamp = ctx.block?.time
+    ctx.eventLogger.emit('troveAdjusted', {
+      user: String(log.data.user),
+      asset_id: String(log.data.asset_id),
+      collateral_change: String(log.data.collateral_change),
+      is_debt_increase: String(log.data.is_debt_increase),
+      debt_change: String(log.data.debt_change),
+      timestamp: String(timestamp),
+      total_collateral: String(log.data.total_collateral),
+      total_debt: String(log.data.total_debt)
+    })
   })
